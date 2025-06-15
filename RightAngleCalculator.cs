9d1c18f48c796de0546cccf7e40e95cc7884c316
MbalieP <mbphulwjhb024@student.wethinkcode.co.za>
2025-06-15 11:26:13 +0200

    Create RightAngleCalculator.cs

---
+++ RightAngleCalculator.cs
@@ -0,0 +1,24 @@
+using System;
+
+
+namespace MyRightAngleApp
+{
+    public class RightAngle
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter side A: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter side B: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double c = Math.Sqrt((a * a) + (b * b));
+
+            Console.WriteLine("The hypotenuse is: " + c);
+
+            Console.ReadKey();
+        }
+    }
+}
